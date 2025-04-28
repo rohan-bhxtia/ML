91e57df7eeda834f5c87cd4494b966641dd116d1
Rohan-bhxtia <rohanbhatia003@gmail.com>
2025-04-28 15:00:09 +0530

    ..

---
+++ structure/6.importModules/packages/__init__.py
@@ -0,0 +1,2 @@
+# this is a special type of file whihc helps in importing packages to other files 
+# by initiating them
---
+++ structure/6.importModules/packages/functions.py
@@ -0,0 +1,15 @@
+# here ill write different functions which we'll use in other files 
+
+
+def add(a,b):
+    return a+b
+
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
